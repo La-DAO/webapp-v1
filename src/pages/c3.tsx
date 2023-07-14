acc45a41010ae88063cbff5a86625da190afdd11
Mel <angel.melendez.c@gmail.com>
2023-07-13 20:00:50 -0600

    create C3 page, install framer-motion

---
+++ src/pages/c3.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const c3 = () => {
+  return (
+    <div className="flex min-h-[calc(100vh-64px)] w-full">
+      <div className="h-full w-1/3 bg-ldJetBlack pt-16 text-ldBeigeWhite">
+        <h3 className="text-2xl font-bold">C3</h3>
+        <li>Conecta</li>
+        <li>Contribuye</li>
+        <li>Construye</li>
+      </div>
+      <div className="min-h-full w-2/3 overflow-y-scroll bg-ldBeigeWhite pt-16">
+        <div className="mx-auto max-w-7xl px-6 md:max-w-2xl lg:max-w-5xl lg:px-8">
+          <h1 className="mb-8 text-center text-4xl font-bold leading-tight tracking-tight text-primary">
+            C3
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default c3;
