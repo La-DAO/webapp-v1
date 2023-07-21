761a246b5fcc5fdb8a445d340fc83b89b51081a7
Mel <angel.melendez.c@gmail.com>
2023-07-21 14:06:40 -0600

    add url to models, create project api router

--- src/server/api/routers/projects.ts
+++ src/server/api/routers/projects.ts
@@ -5,4 +5,41 @@ export const projectsRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.project.findMany();
   }),
+  create: publicProcedure
+    .input(
+      z.object({
+        email: z.string().email().trim(),
+        name: z.string().trim().min(1).max(32),
+        description: z.string().trim().min(20).max(140),
+        isValidated: z.boolean().optional().default(false),
+        tags: z.string().array().min(1),
+        urlBanner: z.string().max(255).trim().optional(),
+        urlProfile: z.string().max(255).trim().optional(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const {
+        email,
+        name,
+        description,
+        isValidated,
+        tags,
+        urlBanner,
+        urlProfile,
+      } = input;
+
+      const project = await ctx.prisma.project.create({
+        data: {
+          email,
+          name,
+          description,
+          isValidated,
+          tags,
+          urlBanner,
+          urlProfile,
+        },
+      });
+
+      return project;
+    }),
 });
